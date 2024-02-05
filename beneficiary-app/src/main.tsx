06ab451362dfa21f40b03ea48f7e2723193c7aba
yashashkumar <yashashk@sanketika.in>
2024-02-05 10:45:56 +0530

    Issue #0000 fix: removed base route

--- beneficiary-app/src/main.tsx
+++ beneficiary-app/src/main.tsx
@@ -8,7 +8,7 @@ import './simple-datatables.css';
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <BrowserRouter basename="/beneficiary-otp">
+    <BrowserRouter >
       <App />
     </BrowserRouter>
 );
