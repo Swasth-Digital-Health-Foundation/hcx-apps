b7b0f6eae11980a7a8651b7ed46f4fdb7ef5299f
Abhishek Gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2024-02-05 18:20:41 +0530

    Fix : opd provider fix

--- opd-app/src/main.tsx
+++ opd-app/src/main.tsx
@@ -8,7 +8,7 @@ import './simple-datatables.css';
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter basename='/provider-opd'>
+  <BrowserRouter basename='/opd-provider'>
     <App />
   </BrowserRouter>
 );
