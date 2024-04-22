561d0bb86a03e5c69a5f1eb9e637d6427277c811
Abhishek Gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2024-04-22 19:39:18 +0530

    Feat : Added remarks & approved amount

--- beneficiary-app/src/pages/SendBankDetails/SendBankDetails.tsx
+++ beneficiary-app/src/pages/SendBankDetails/SendBankDetails.tsx
@@ -170,18 +170,48 @@ const SendBankDetails = () => {
             {strings.TREATMENT_AND_BILLING_DETAILS}
           </h2>
         </div>
-        <div>
-          {_.map(treatmentDetails, (ele: any) => {
-            return (
-              <div className="flex gap-2">
-                <h2 className="text-bold text-base font-bold text-black dark:text-white">
-                  {ele.key}
-                </h2>
-                <span className="text-base font-medium">INR {ele.value}</span>
-              </div>
-            );
-          })}
+        <div className=" gap-2">
+          <div className='flex'>
+            <h2 className="text-bold text-base font-bold text-black dark:text-white">
+              {"Service type :"}
+            </h2>
+            <span className="text-base ml-15.5 font-medium">{details?.serviceType}</span>
+          </div>
+          <div className='flex'>
+            <h2 className="text-bold text-base font-bold text-black dark:text-white">
+              {"Bill amount :"}
+            </h2>
+            <span className="text-base ml-18 font-medium">INR {details?.billAmount}</span>
+          </div>
+          <div className='flex'>
+            {beneficiaryBankDetails[0]?.status === "response.complete" ? <>
+              <h2 className="text-bold text-base  font-bold text-black dark:text-white">
+                {"Approved amount :"}
+              </h2>
+              <span className="text-base ml-5 font-medium">INR {beneficiaryBankDetails[0]?.approvedAmount}</span>
+            </> : <></>
+            }</div>
         </div>
+        {beneficiaryBankDetails[0]?.status === "response.complete" && beneficiaryBankDetails[0]?.remarks !== "" ?
+          <>
+            <div className="flex items-center justify-between">
+              <h2 className="sm:text-title-xl1 text-1xl mt-2 mb-4 font-semibold text-black dark:text-white">
+                <span className="flex items-center">
+                  {"Remarks"}
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 ml-2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+                  </svg>
+                </span>
+              </h2>
+            </div>
+            <div className="flex gap-2">
+              <h2 className="">
+                {beneficiaryBankDetails[0]?.remarks}
+              </h2>
+            </div>
+          </> : <></>
+        }
+
       </div>
       {isConsentVerified || beneficiaryBankDetails[0]?.otpStatus === 'successful' ?
         <div className="mt-2 p-2 rounded-lg border border-stroke bg-white px-3 shadow-default dark:border-strokedark dark:bg-boxdark">
@@ -215,7 +245,6 @@ const SendBankDetails = () => {
             </div>
           </div> : <></>
       }
-
       {beneficiaryBankDetails[0]?.bankStatus === 'successful' ?
         <button
           onClick={(event: any) => {
