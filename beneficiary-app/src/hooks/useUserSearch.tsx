1686e504d46fdc5f22014c43affe4aad2523bb8d
jagadishkesav <s.jagadishkpd@gmail.com>
2025-06-16 10:52:42 +0530

    Create a custom hook for user search

---
+++ beneficiary-app/src/hooks/useUserSearch.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import { searchUser } from '../services/hcxMockService';
+import { useLocation } from 'react-router-dom';
+import { isEmpty } from 'lodash';
+
+export const useUserSearch = (mobile: string | null) => {
+    const location = useLocation();
+    
+    const [userInfo, setUserInfo] = useState<any[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<any>(null);
+
+    useEffect(() => {
+        if (!mobile) return;
+
+        const fetchData = async () => {
+            setLoading(true);
+            setError(null);
+            try {
+                const response = await searchUser('user/search', mobile);
+                if (location && !isEmpty(location.state) && 'insuranceId' in location.state) {
+                    const filteredObject = response.data.find((ele: any) => ele.payorDetails.length > 0 && ele.payorDetails[0].insurance_id === location.state.insuranceId);
+                    if (filteredObject) {
+                        setUserInfo([filteredObject]);
+                    }
+                } else {
+                    setUserInfo(response.data);
+                }
+            } catch (err) {
+                setError(err);
+                setUserInfo([]);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [mobile]);
+
+    return { userInfo, loading, error };
+};
