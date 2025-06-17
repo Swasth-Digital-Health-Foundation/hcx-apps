c0afc5070e2668514d8391457bafc3260c7a01f9
jagadishkesav <s.jagadishkpd@gmail.com>
2025-06-17 08:53:29 +0530

    Create re-usable date time formatters

---
+++ beneficiary-app/src/utils/index.ts
@@ -0,0 +1,30 @@
+export const formatTime = (dateString: string | number) => {
+  const date = new Date(dateString);
+  return date.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true,
+  });
+};
+export const formatDate = (dateString: string | number) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
+};
+
+export const formatDateTime = (dateString: string | number) => {
+  const date = new Date(dateString);
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  };
+
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+};
